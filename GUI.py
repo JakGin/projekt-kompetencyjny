1d2ffd401b4b7f91337f2e09ca2b8603e6455c80
Kacper Zatoński <240844@edu.p.lodz.pl>
2024-06-05 12:52:00 +0200

    Changed button.

--- GUI.py
+++ GUI.py
@@ -7,18 +7,62 @@ import cnn_to_app as cnn
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
+        super().__init__()
         self.save_path = "./image"
         self.img = None
-        super().__init__()
         self.filename = ""
-
+        self.setStyleSheet("""
+                    QWidget#myCustomWidget {
+                        background-color: #f0f0f0; /* Light grey */
+                        padding: 15px; /* Padding inside the widget */
+                    }
+                    QLabel {
+                        color: black; /* Dark grey text color */
+                        font-size: 30px;
+                    }
+                    QPushButton {
+                        background-color: #d3d3d3; 
+    border: none; 
+    border-radius: 15px; 
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); 
+    font-size: 20px; 
+    color: #000; /* Text color */
+    text-align: center; /* Center the text */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer; /* Pointer cursor on hover */
+    transition: background-color 0.3s ease; 
+                        text-align: center;
+                        font-size: 30px;
+                    }
+                    QPushButton:hover {
+                        background-color: #c0c0c0;
+                        color: black;
+                        border: 2px solid #171717;
+                    }
+                """)
         self.inputButton = QtWidgets.QPushButton("")
         self.analyzeButton = QtWidgets.QPushButton("Analyze image.")
+        self.analyzeButton.setStyleSheet("""
+        QPushButton {
+            position: absolute;
+            width: 745px;
+            height: 66.5px;
+        }
+        """)
         self.text = QtWidgets.QLabel("Input image to analyze.", alignment=QtCore.Qt.AlignCenter)
         self.pixmap = QtGui.QPixmap()
 
         self.inputButton.setFixedSize(500, 500)
         self.inputButton.setIconSize(QtCore.QSize(400, 400))
+        self.inputButton.setStyleSheet(("""
+            QPushButton {
+                position: absolute;
+                width: 495px;
+                height: 495px;
+            }
+        """))
         self.inputButton.setIcon(QtGui.QIcon("./image.png"))
 
         self.layout = QtWidgets.QVBoxLayout(self)
@@ -53,7 +97,6 @@ class MyWidget(QtWidgets.QWidget):
         img_byte_arr.seek(0)
         self.pixmap.loadFromData(img_byte_arr.read())
         self.inputButton.setIcon(QtGui.QIcon(self.pixmap))
-        self.img.save(self.save_path + "/" + self.filename.split('/')[-1])
 
 
 if __name__ == "__main__":
